659d7f6815c739ab5751b38893e005c4939e7389
Harhsit Vishwakarma <130080307+HarryVosh@users.noreply.github.com>
2024-05-02 13:10:47 +0530

    Add on in contact section

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function color(){
 	var body = document.getElementById("bg");
 	body.bgColor = "black";
 }
+
+
+// This is for feedback form only //
+
+function SendMail(){
+	var params = {
+		from_name : document.getElementById("fullName").value,
+		email_id : document.getElementById("email_id").value,
+		subject : document.getElementById("subject").value,
+		message  : document.getElementById("message").value
+	}
+	emailjs.send("service_wp0uil8", "template_om26i9y", params).then(function(res){
+		alert("Message send successfully! " + res.status);
+	})
+}
